f2f5d359e1c2442473f159c2ead4920ceddd9222
synt4x0r <mohmd.atia33@gmail.com>
2025-01-06 20:30:48 +0200

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // string Formatting
+            // Eq : x + y = 12
+
+            int x = 3, y = 4;
+
+            // 1. string concatantaion
+
+            //string message = "Eq : " + x + " + " + y + " = " + (x + y);
+
+            //Console.WriteLine(message);
+
+            // string : Immutable datatype > cant change its value after creation
+
+            // 2. string format
+
+            //string message = string.Format("Eq : {0} + {1} + = {2}", x, y, x + y);
+
+            //Console.WriteLine(message);
+
+            // 2. string interpolution
+            //$ > string manipulation
+
+            //string message = $"Eq : {x} + {y} = {x + y}";
+
+            //Console.WriteLine(message);
+
+        }
+    }
+}
