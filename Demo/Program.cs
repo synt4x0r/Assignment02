df39c34284366ebb50866436efe23e27028b7da9
synt4x0r <mohmd.atia33@gmail.com>
2025-01-12 17:54:39 +0200

    Control Statements [Numeric Datatype]

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,10 +4,11 @@
     {
         static void Main(string[] args)
         {
+            #region string formatting
             // string Formatting
             // Eq : x + y = 12
 
-            int x = 3, y = 4;
+            //int x = 3, y = 4;
 
             // 1. string concatantaion
 
@@ -29,8 +30,113 @@
             //string message = $"Eq : {x} + {y} = {x + y}";
 
             //Console.WriteLine(message);
-            //
+            // 
+            #endregion
 
+            #region control statement
+
+            // conditional control statement (if statement, switch case)
+            // loop control statement (for, foreach, while, do-while)
+
+            //Console.WriteLine("hello world");
+            //Console.WriteLine("hello world");
+            //Console.WriteLine("hello world");
+
+
+            // conditional control statement (if statement)
+
+            //Console.Write("enter number of month in the 1st quarter : ");
+            //int monum = int.Parse(Console.ReadLine());
+
+            // 1 --> Jan
+            // 2 --> Feb
+            // 3 --> Mar
+
+
+            //Console.WriteLine("Jan");
+            //Console.WriteLine("Feb");
+            //Console.WriteLine("Mar");
+
+            //if (/*Expression*/)
+            //{
+            //    // code
+            //    // this code will be executed when the condition is true
+            //}
+
+
+            //if (true)
+            //{
+            //    Console.WriteLine("hello world");
+
+            //}
+
+            // using if
+
+
+            //while (true)
+            //{
+                //Console.Write("enter number of month in the 1st quarter : ");
+                //int monum = int.Parse(Console.ReadLine());
+
+            //if (monum == 1)
+            //{
+            //    Console.WriteLine("Jan");
+            //}
+            //if (monum == 2)
+            //{
+            //    Console.WriteLine("Feb");
+            //}
+            //if (monum == 3)
+            //{
+            //    Console.WriteLine("Mar");
+            ////}
+
+
+            
+
+            // using switch
+
+            //switch (/*Expression*/)
+            //{
+            //    case
+            //}
+
+            
+            while (true)
+            {
+                Console.Write("enter number of month in the 1st quarter : ");
+                int monum = int.Parse(Console.ReadLine());
+                // jump table
+                switch (monum)
+                {
+                    case 1:
+                        Console.WriteLine("Jan");
+                        break;
+                    case 2:
+                        Console.WriteLine("Feb");
+                        break;
+                    case 3:
+                        Console.WriteLine("Mar");
+                        break;
+                    default:
+                        Console.WriteLine("invalid month number");
+                        break;
+                    case 5:
+                        Console.WriteLine("May");
+                        break;
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+            #endregion
         }
     }
 }
